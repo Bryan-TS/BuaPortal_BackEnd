e2ba00565aa98bab43b158e09257ecf79863c412
unknown <bryants.284@gmail.com>
2022-04-24 15:23:10 -0500

    First version crud to Users

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,62 +3,70 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    public function login(Request $request){
+        $user =     User::select('id','email','name','lastName','description')
+                    ->where('email',$request->email)
+                    ->where('password',$request->password)
+                    ->first();
+
+        if($user !== null){
+            // $token = hash('sha256', $user->email);
+            return $user;
+        }else{
+            return $response['status'] = "401";
+        }
+
+        
+    }
+
+
     public function index()
     {
-        return "Hola soy el index";
+        $users = User::all();
+        return $users;
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        return "Tu email es: " . $request->email;
+        $user = new User();
+        $user->name = $request->name;
+        $user->lastName = $request->lastName;
+        $user->description = $request->description;
+        $user->email = $request->email;
+        $user->password = $request->password;
+
+        $user->save();
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function show($id)
     {
-        return "El id solicitado es". $id;
+        $user = User::find($id);
+        return $user;
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
-        //
+        $user = User::findOrFail($id);
+        $user->name = $request->name;
+        $user->lastName = $request->lastName;
+        $user->description = $request->description;
+        $user->email = $request->email;
+        $user->password = $request->password;
+
+        $user->save();
+
+        return $user;
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        //
+        $user = User::destroy($id);
+        return $user;
     }
 }
